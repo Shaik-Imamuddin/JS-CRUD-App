ffbfa850504bf7de87b459b379388ee57af328e3
Immu <shaikimam37302@gmail.com>
2025-08-26 00:03:55 +0530

    Javascript CRUD-APP

--- script.js
+++ script.js
@@ -1,82 +1,71 @@
-var selecetedRow = null;
+const taskinput = document.getElementById("task-input");
+const addtaksbtn = document.getElementById("add-task-btn")
+const tasklist = document.getElementById("task-list")
+const searchinput = document.getElementById("search-input")
 
-//show alerts
-function showAlert(message , className){
-    const div = document.createElement("div");
-    div.className = `alert alert-${className}`;
+// add task
 
-    div.appendChild(document.createTextNode(message));
-    const container = document.querySelector(".container");
-    const main = document.querySelector(".main");
-    container.insertBefore(div ,main);
-
-    setTimeout(()=>document.querySelector(".alert").remove(),3000);
-}
-
-//Clear all fields
-function clearFields(){
-    document.querySelector('#firstName').value = "";
-    document.querySelector('#lastName').value = "";
-    document.querySelector('#rollNo').value = "";
-}
-
-// Add Data
-document.querySelector("#student-form").addEventListener("submit", (e)=>{
-    e.preventDefault();
-    //get Form values
-    const firstName = document.querySelector("#firstName").value;
-    const lastName = document.querySelector("#lastName").value;
-    const rollNo = document.querySelector("#rollNo").value;
-
-    //validate
-    if(firstName == "" || lastName == "" || rollNo == ""){
-        showAlert("Please fill in all fields", "danger");
+addtaksbtn.addEventListener("click",()=>{
+    const taskText = taskinput.value.trim();
+    if(taskText === ""){
+        alert("Please Enter a Task")
+        return
     }
-    else{
-        if(selecetedRow == null){
-            const list = document.querySelector("#student-list");
-            const row = document.createElement("tr");
 
-            row.innerHTML = `
-                <td>${firstName}</td>
-                <td>${lastName}</td>
-                <td>${rollNo}</td>
-                <td>
-                <a href="#" class="btn btn-warning btn-sm edit">Edit</a>
-                <a href="#" class="btn btn-danger btn-sm delete">Delete</a></td>
-            `;
-            list.appendChild(row);
-            selecetedRow = null;
-            showAlert("Student Added","sucess");
+    const taskitem  =  document.createElement("li")
+    taskitem.className = "task-item"
+
+    const taskTextInput = document.createElement("input")
+    taskTextInput.type = "text";
+    taskTextInput.value = taskText;
+    taskTextInput.disabled = true;
+
+    // edit btn
+    const editbtn = document.createElement("button");
+    editbtn.textContent = "Edit";
+    editbtn.className = "edit-btn";
+
+    //delete btn
+    const deletebtn = document.createElement("button");
+    deletebtn.textContent = "Delete";
+    deletebtn.className = "delete-btn";
+
+    taskitem.appendChild(taskTextInput)
+    taskitem.appendChild(editbtn)
+    taskitem.appendChild(deletebtn)
+    tasklist.appendChild(taskitem)
+
+
+    taskinput.value = "";
+
+    editbtn.addEventListener("click",()=>{
+        if(editbtn.textContent === "Edit"){
+            taskTextInput.disabled = false
+            editbtn.textContent ="Save"
         }
         else{
-            selecetedRow.children[0].textContent = firstName;
-            selecetedRow.children[1].textContent = lastName;
-            selecetedRow.children[2].textContent = rollNo;
-            selecetedRow = null;
-            showAlert("student Info Edited" , "Info")
+            taskTextInput.disabled = true
+            editbtn.textContent ="Edit"
         }
-        clearFields();
-    }
+    })
 
-});
+    deletebtn.addEventListener("click",()=>{
+        tasklist.removeChild(taskitem)
+    })
+})
 
-// Edit Data
-document.querySelector("#student-list").addEventListener("click",(e)=>{
-    target=e.target;
-    if(target.classList.contains("edit")){
-        selecetedRow = target.parentElement.parentElement;
-        document.querySelector("#firstName").value = selecetedRow.children[0].textContent;
-        document.querySelector("#lastName").value = selecetedRow.children[1].textContent;
-        document.querySelector("#rollNo").value = selecetedRow.children[2].textContent;
-    }
-});
+searchinput.addEventListener("input" , ()=>{
+    const searchValue = searchinput.value.toLowerCase();
 
-//delete data
-document.querySelector('#student-list').addEventListener("click",(e)=>{
-  target = e.target;
-  if(target.classList.contains("delete")){
-    target.parentElement.parentElement.remove();
-    showAlert("Student Data Deleted","danger");
-  }  
-});
+    const tasks = tasklist.getElementsByClassName("task-item")
+
+    Array.from(tasks).forEach(task=>{
+        const tasktext = task.querySelector("input[type='text']").value.toLowerCase()
+        if(tasktext.includes(searchValue)){
+            task.style.display = "";
+        }
+        else{
+            task.style.display = "none";
+        }
+    })
+})
